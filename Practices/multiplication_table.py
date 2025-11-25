f241027fddcb54472933546aa623552e3de0ee17
m-subhan-tahir <subhantahir430@gmail.com>
2025-11-25 21:01:52 +0500

    change hardcoded end value into dynamic user dependent

--- Practices/multiplication_table.py
+++ Practices/multiplication_table.py
@@ -1,7 +1,8 @@
 print("Multiplication table")
 num = int(input("Enter your table number: "))
+endNum = int(input("Enter your ending number: "))
 sum = 0
-for i in range(1,11):
+for i in range(1,endNum+1):
     print(f"{num} * {i} = {i * num}")
     sum += (i*num)
 print(f"\nSum of {num} * 1...10 = {sum}")    
