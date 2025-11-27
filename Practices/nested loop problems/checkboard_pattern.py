8616a6f1524af178e1815ada00887a1de754b389
m-subhan-tahir <subhantahir430@gmail.com>
2025-11-27 19:40:41 +0500

    make programs:checkboard-pattern,print-the-no of rows and print-the-no of columns

---
+++ Practices/nested loop problems/checkboard_pattern.py
@@ -0,0 +1,34 @@
+# Write the algorithm of checkboard pattern.
+# step 1:create a outer loop for print the rows. /done
+# step 2:Inside each row make the columns using inner loop
+# werwer 3:print the X and make third loop for print O 4 divided by 2
+
+
+
+# rows = 5
+# columns = 5
+# for i in range(rows):
+#     for j in range(columns):
+#         if (i + j) % 2 == 0:
+#             print("X",end=" ")
+#         else:
+#             print("O",end=" ")   
+#     print()         
+            
+                  
+ # Make the program print the no of rows and columns from the user input
+ 
+# userRows = int(input("Enter no of rows: "))                                                                                                                                                                                                                                     
+# userColumns = int(input("Enter no of columns: ")) 
+ 
+# for k in range(1,userRows+1):
+#     print(f"Row {k}:",end=" ")
+#     for l in range(1,userColumns+1):
+#         print(f"Column {l}", end=" ")
+#     print()        
+ 
+# print coordinates using nested loop
+for m in range(1,4):
+    for n in range(1,4):
+        print(f"({m},{n})",end=" ")
+    print()                                                                                                                                                                                                                                            
